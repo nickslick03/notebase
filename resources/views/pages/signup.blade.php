8ff698a1aba1de06354267167c4c6b0c823bc3b0
Andrea <andreaguiher@yahoo.com>
2024-12-05 14:35:20 -0500

    added signup page code

--- resources/views/pages/signup.blade.php
+++ resources/views/pages/signup.blade.php
@@ -5,7 +5,48 @@
 @endsection
 
 @section('body-content')
-    @include('partials.header')
-    <h1>Sign up</h1>
+<body>
+    <div id = "topBar">
+          <table>
+              <td id = "mainText">Register for Notebase!</td>
+              <td id = "space"></td>
+              <td><a class = "nav" href ="login">Login</a></td>
+          </table>
+      </div>
+    <br>
+    <br>
+    <form action="" method = "POST">
+    <!--Capturing the Username-->
+    <label for="uname">Username:</label>
+  <input type="text" id="uname" name="uname">
+    <br>
+    <br>
+    <!--Capturing the Email-->
+    <label for="email">Email:</label>
+    <input type="text" id="email" name="email">
+    <br>
+    <br>
+    <!--Capturing the Role-->
+    <select name="role" id="role">
+        <option value="">--- Choose a role ---</option>
+        <option value="profesor">Professor</option>
+        <option value="student">Student</option>
+    </select>
+    <br>
+    <br>
+    <!--Capturing the Password-->
+    <label for="pwd">Password:</label>
+    <input type="password" id="pwd" name="pwd">
+    <br>
+    <br>
+    <!--Capturing the Match Password-->
+    <label for="mtchpwd">Confirm Password:</label>
+    <input type="password" id="mtchpwd" name="mtchpwd">
+    <br>
+    <br>
+    <input type="submit" value="Submit">
+    </form>
+    </body>
+    </html>
     @include('partials.footer')
 @endsection
