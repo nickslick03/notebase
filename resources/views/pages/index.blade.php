64a8ea5a7662e3b9b19e14e0495cc34954208417
Andrea <andreaguiher@yahoo.com>
2024-12-05 12:56:01 -0500

    landing page added to php and css

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -5,7 +5,25 @@
 @endsection
 
 @section('body-content')
-    @include('partials.header')
-    <h1>Welcome to notebase</h1>
+<html>
+    <head>
+    <style>
+    <link rel="stylesheet" href="css/index.css">
+    </style>
+    </head>
+        <table id = "topBar" width = "100%">
+            <td id = "mainText">Welcome to Notebase!</td>
+            <td><a class = "nav" href ="login">Login</a></td>
+        </table>
+    </div>
+    <br>
+    <br>
+    <br>
+    <p id = "quote">"Without Notebase I never would've passed my econ class!" - University Student</p>
+    <span id = "button"><a href = "signup"><button type="button">Register Here!</button></a></span>
+    <br>
+    <br>
+   
+ </html>
     @include('partials.footer')
 @endsection
