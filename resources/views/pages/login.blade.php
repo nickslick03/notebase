ce3c04c5991501e5ffdcc06817a613a48dfdf572
Andrea <andreaguiher@yahoo.com>
2024-12-05 14:34:44 -0500

    added login page code

--- resources/views/pages/login.blade.php
+++ resources/views/pages/login.blade.php
@@ -5,7 +5,30 @@
 @endsection
 
 @section('body-content')
-    @include('partials.header')
-    <h1>Log In</h1>
+    <body><div id = "topBar">
+        <table>
+            <td id = "mainText">Notebase</td>
+            <td id = "space"></td>
+            <td><a class = "nav" href ="signup">Register</a></td>
+        </table>
+    </div>
+    <br>
+    <br></body>
+    <br>
+    <form action="" method = "POST">
+    <!--Capturing the Username-->
+    <label for="uname">Username:</label>
+    <input type="text" id="uname" name="uname">
+    <br>
+    <br>
+    <!--Capturing the Password-->
+    <label for="pwd">Password:</label>
+    <input type="password" id="pwd" name="pwd">
+    <br>
+    <br>
+    <input type="submit" value="Submit">
+    </form>
+    </body>
+    </html>
     @include('partials.footer')
 @endsection
