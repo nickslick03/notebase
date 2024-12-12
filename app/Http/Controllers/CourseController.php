b6baa0465d63913fc371d5481f21d7a38929ceb0
nicholas <91232026+nickslick03@users.noreply.github.com>
2024-12-11 22:46:26 -0500

    added login url callback functionality

--- app/Http/Controllers/CourseController.php
+++ app/Http/Controllers/CourseController.php
@@ -8,9 +8,9 @@ use Illuminate\Support\Facades\DB;
 
 class CourseController extends Controller
 {
-    public function index() {
+    public function index(Request $request) {
         if (!session()->has('user')) {
-            return redirect('login');
+            return redirect('login?callback_url=' . base64_encode($request->url()));
         }
 
         $courses = DB::select('select * from course_view');
--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,14 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
-
 
 class DashboardController extends Controller
 {
-    public function index() {
+    public function index(Request $request) {
         if (!session()->has('user')) {
-            return redirect('login');
+            return redirect('login?callback_url=' . base64_encode($request->url()));
         }
         return view('pages.dashboard');
     }
