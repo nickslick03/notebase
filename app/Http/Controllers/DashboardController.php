2c8af38a5f2bceadd154dad6e2f8f9b4a71bffb7
nicholas <91232026+nickslick03@users.noreply.github.com>
2024-12-12 19:55:04 -0500

    made dashboard dynamic

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class DashboardController extends Controller
 {
@@ -10,6 +11,24 @@ class DashboardController extends Controller
         if (!session()->has('user')) {
             return redirect('login?callback_path=' . base64_encode($request->getPathInfo()));
         }
-        return view('pages.dashboard');
+
+        $courses = DB::select('
+        select * from course_view cv
+        join associated_course_user acu
+            on cv.course = acu.course and acu.user = ?',
+        [session()->get('user')->user]);
+
+        $starred_resources = DB::select("
+        select r.*, concat(subject_code, ' ', course_code) as code from resource r
+        join starred_resource_user sru
+            on r.resource = sru.resource and user = ?
+        join course_view cv
+            on cv.course = r.course;",
+        [session()->get('user')->user]);
+
+        return view('pages.dashboard', [
+            'courses' => $courses,
+            'starred_resources' => $starred_resources
+        ]);
     }
 }
--- resources/views/pages/dashboard.blade.php
+++ resources/views/pages/dashboard.blade.php
@@ -13,83 +13,23 @@
                 <!-- Cards -->
                 <div style="display: flex; align-items: center;">
                     <div style="display: flex; overflow-x: scroll; width: 100%;">
-                        <div class="course-card">
-                            <div class="card-photo">
-                                <img class="card-img">
-                            </div>
-                            <div class="card-text">
-                                <div style="margin: auto;">
-                                    <b><div>Introductory Statistics</div></b>
-                                    <div>STAT 269</div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="course-card">
-                            <div class="card-photo">
-                                <img class="card-img">
-                            </div>
-                            <div class="card-text">
-                                <div style="margin: auto;">
-                                    <b><div>Intermediate Spanish</div></b>
-                                    <div>SPAN 201</div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="course-card">
-                            <div class="card-photo">
-                                <img class="card-img">
-                            </div>
-                            <div class="card-text">
-                                <div style="margin: auto;">
-                                    <b><div>Drawing I</div></b>
-                                    <div>ART 171</div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="course-card">
-                            <div class="card-photo">
-                                <img class="card-img">
-                            </div>
-                            <div class="card-text">
-                                <div style="margin: auto;">
-                                    <b><div>Programming for User Interaction</div></b>
-                                    <div>CIS 281</div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="course-card">
-                            <div class="card-photo">
-                                <img class="card-img">
-                            </div>
-                            <div class="card-text">
-                                <div style="margin: auto;">
-                                    <b><div>Web Development: Server Side</div></b>
-                                    <div>CIS 291</div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="course-card">
-                            <div class="card-photo">
-                                <img class="card-img">
-                            </div>
-                            <div class="card-text">
-                                <div style="margin: auto;">
-                                    <b><div>Web Development: Server Side</div></b>
-                                    <div>CIS 291</div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="course-card">
-                            <div class="card-photo">
-                                <img class="card-img">
-                            </div>
-                            <div class="card-text">
-                                <div style="margin: auto;">
-                                    <b><div>Web Development: Server Side</div></b>
-                                    <div>CIS 291</div>
-                                </div>
-                            </div>
-                        </div>
+                        @isset($courses)
+                            @foreach ($courses as $course)
+                                <a href="/course/{{ $course->course }}">
+                                    <div class="course-card">
+                                        <div class="card-photo">
+                                            <img class="card-img">
+                                        </div>
+                                        <div class="card-text">
+                                            <div style="margin: auto;">
+                                                <b><div>{{ $course->title }}</div></b>
+                                                <div>{{ $course->subject_code . ' ' . $course->course_code . ' ' . $course->title }}</div>
+                                            </div>
+                                        </div>
+                                    </div>    
+                                </a>
+                            @endforeach
+                        @endisset
                     </div>
                 </div>
             </section>
@@ -110,78 +50,23 @@
                             </div>
                             <img class="note-img" src="https://wiki.theplaz.com/w/images/thumb/American_Studies_Chap_17_-_Reconstruction_-_Politics_of_Reconstruction_Page_1.jpg/300px-American_Studies_Chap_17_-_Reconstruction_-_Politics_of_Reconstruction_Page_1.jpg">
                         </div>
-                        <div class="note-preview">
-                            <div class="note-information">
-                                <div style="background: white; width: 35px; border-radius: 20px;">
-                                    <iron-icon icon="star" style="padding: 5px; color: var(--main-color);"></iron-icon>
+                        @isset($starred_resources)
+                            @foreach ($starred_resources as $resource)
+                                <div class="note-preview">
+                                    <div class="note-information">
+                                        <div style="background: white; width: 35px; border-radius: 20px;">
+                                            <iron-icon icon="star" style="padding: 5px; color: var(--main-color);"></iron-icon>
+                                        </div>
+                                        <div style="padding: 0 12px; display: flex; margin: auto;">
+                                            <div style="margin-right: 4px;">{{ $resource->code }}</div>
+                                            <div>{{ $resource->title }}</div>
+                                        </div>
+                                    </div>
+                                    <img class="note-img" src="https://wiki.theplaz.com/w/images/thumb/American_Studies_Chap_17_-_Reconstruction_-_Politics_of_Reconstruction_Page_1.jpg/300px-American_Studies_Chap_17_-_Reconstruction_-_Politics_of_Reconstruction_Page_1.jpg">
                                 </div>
-                                <div style="padding: 0 12px; display: flex; margin: auto;">
-                                    <div style="margin-right: 4px;">CIS 281</div>
-                                    <div>Chapter 14</div>
-                                </div>
-                            </div>
-                            <img class="note-img" src="https://wiki.theplaz.com/w/images/thumb/American_Studies_Chap_17_-_Reconstruction_-_Politics_of_Reconstruction_Page_1.jpg/300px-American_Studies_Chap_17_-_Reconstruction_-_Politics_of_Reconstruction_Page_1.jpg">
-                        </div>
-                        <div class="note-preview">
-                            <div class="note-information">
-                                <div style="background: white; width: 35px; border-radius: 20px;">
-                                    <iron-icon icon="star" style="padding: 5px; color: var(--main-color);"></iron-icon>
-                                </div>
-                                <div style="padding: 0 12px; display: flex; margin: auto;">
-                                    <div style="margin-right: 4px;">CIS 281</div>
-                                    <div>Chapter 14</div>
-                                </div>
-                            </div>
-                            <img class="note-img" src="https://wiki.theplaz.com/w/images/thumb/American_Studies_Chap_17_-_Reconstruction_-_Politics_of_Reconstruction_Page_1.jpg/300px-American_Studies_Chap_17_-_Reconstruction_-_Politics_of_Reconstruction_Page_1.jpg">
-                        </div>
-                        <div class="note-preview">
-                            <div class="note-information">
-                                <div style="background: white; width: 35px; border-radius: 20px;">
-                                    <iron-icon icon="star" style="padding: 5px; color: var(--main-color);"></iron-icon>
-                                </div>
-                                <div style="padding: 0 12px; display: flex; margin: auto;">
-                                    <div style="margin-right: 4px;">CIS 281</div>
-                                    <div>Chapter 14</div>
-                                </div>
-                            </div>
-                            <img class="note-img" src="https://wiki.theplaz.com/w/images/thumb/American_Studies_Chap_17_-_Reconstruction_-_Politics_of_Reconstruction_Page_1.jpg/300px-American_Studies_Chap_17_-_Reconstruction_-_Politics_of_Reconstruction_Page_1.jpg">
-                        </div>
-                        <div class="note-preview">
-                            <div class="note-information">
-                                <div style="background: white; width: 35px; border-radius: 20px;">
-                                    <iron-icon icon="star" style="padding: 5px; color: var(--main-color);"></iron-icon>
-                                </div>
-                                <div style="padding: 0 12px; display: flex; margin: auto;">
-                                    <div style="margin-right: 4px;">CIS 281</div>
-                                    <div>Chapter 14</div>
-                                </div>
-                            </div>
-                            <img class="note-img" src="https://wiki.theplaz.com/w/images/thumb/American_Studies_Chap_17_-_Reconstruction_-_Politics_of_Reconstruction_Page_1.jpg/300px-American_Studies_Chap_17_-_Reconstruction_-_Politics_of_Reconstruction_Page_1.jpg">
-                        </div>
-                        <div class="note-preview">
-                            <div class="note-information">
-                                <div style="background: white; width: 35px; border-radius: 20px;">
-                                    <iron-icon icon="star" style="padding: 5px; color: var(--main-color);"></iron-icon>
-                                </div>
-                                <div style="padding: 0 12px; display: flex; margin: auto;">
-                                    <div style="margin-right: 4px;">CIS 281</div>
-                                    <div>Chapter 14</div>
-                                </div>
-                            </div>
-                            <img class="note-img" src="https://wiki.theplaz.com/w/images/thumb/American_Studies_Chap_17_-_Reconstruction_-_Politics_of_Reconstruction_Page_1.jpg/300px-American_Studies_Chap_17_-_Reconstruction_-_Politics_of_Reconstruction_Page_1.jpg">
-                        </div>
-                        <div class="note-preview">
-                            <div class="note-information">
-                                <div style="background: white; width: 35px; border-radius: 20px;">
-                                    <iron-icon icon="star" style="padding: 5px; color: var(--main-color);"></iron-icon>
-                                </div>
-                                <div style="padding: 0 12px; display: flex; margin: auto;">
-                                    <div style="margin-right: 4px;">CIS 281</div>
-                                    <div>Chapter 14</div>
-                                </div>
-                            </div>
-                            <img class="note-img" src="https://wiki.theplaz.com/w/images/thumb/American_Studies_Chap_17_-_Reconstruction_-_Politics_of_Reconstruction_Page_1.jpg/300px-American_Studies_Chap_17_-_Reconstruction_-_Politics_of_Reconstruction_Page_1.jpg">
-                        </div>
+                            @endforeach
+                        @endisset
+                        
                     </div>
                 </div>
             </section>
