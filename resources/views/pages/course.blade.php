7cde386777eb7a0060dfe706589c531b84bb16b2
Annika <annikapomeroy@gmail.com>
2024-12-13 21:29:26 -0500

    Added empty note info

--- resources/views/pages/course.blade.php
+++ resources/views/pages/course.blade.php
@@ -20,31 +20,38 @@
             <!--Actual Notes-->
             <div class="note-section">
                 <div id = "sectionStyles">
-                    @foreach ($resources as $resource)
-                        <div class="note-preview" style="margin: 12px 24px 24px 24px;" data-resource={{ $resource->resource }}  data-is_author={{ $resource->is_author }}>
-                            <div class="note-information">
-                                <form class="toggle-star" action="/resource/toggle_star" method="post">
-                                    @csrf
-                                    <input type="hidden" name="resource" value="{{ $resource->resource }}">
-                                    <input type="hidden" name="is_starred" value="{{ $resource->is_starred }}">
-                                    <button type="submit">
-                                        <!--Stars-->
-                                        <div class="star-container">
-                                            <iron-icon icon="star-border" style="color: var(--main-color);"></iron-icon>
-                                            <iron-icon icon="star" style="color: var(--main-color);"></iron-icon>
-                                        </div>
-                                    </button>
-                                </form>
-                                <div  class = "title">
-                                    <div>{{ $resource->title }}</div>
+                    @if(count($resources) !== 0)
+                        @foreach ($resources as $resource)
+                            <div class="note-preview" style="margin: 12px 24px 24px 24px;" data-resource={{ $resource->resource }}  data-is_author={{ $resource->is_author }}>
+                                <div class="note-information">
+                                    <form class="toggle-star" action="/resource/toggle_star" method="post">
+                                        @csrf
+                                        <input type="hidden" name="resource" value="{{ $resource->resource }}">
+                                        <input type="hidden" name="is_starred" value="{{ $resource->is_starred }}">
+                                        <button type="submit">
+                                            <!--Stars-->
+                                            <div class="star-container">
+                                                <iron-icon icon="star-border" style="color: var(--main-color);"></iron-icon>
+                                                <iron-icon icon="star" style="color: var(--main-color);"></iron-icon>
+                                            </div>
+                                        </button>
+                                    </form>
+                                    <div  class = "title">
+                                        <div>{{ $resource->title }}</div>
+                                    </div>
+                                </div>
+                                <div class = "noteStuff"></div>
+                                <div class = "imaging">
+                                    <img class="note-img" src="/img/img.png">
                                 </div>
                             </div>
-                            <div class = "noteStuff"></div>
-                            <div class = "imaging">
-                                <img class="note-img" src="/img/img.png">
-                            </div>
+                        @endforeach
+                    @else
+                        <div id="empty-note-warning">
+                            <h2>Oh no! There are no notes uploaded to this class yet!</h2>
+                            <div style="font-style: italic;">Add this course to your dashboard via <a href="/course" style="color: var(--light-text-color);"><b>Courses</b></a> in order to upload notes files within <a href="/upload_resource" style="color: var(--light-text-color);"><b>Add Resource</b></a>.</div>
                         </div>
-                    @endforeach
+                    @endif
                 </div>
             </div>
         </section>
