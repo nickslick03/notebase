6c7daf72c74b0daec8557abf40d6d00c69a722ad
Annika <annikapomeroy@gmail.com>
2024-12-13 19:02:07 -0500

    Fixed download button padding

--- resources/views/partials/modal.blade.php
+++ resources/views/partials/modal.blade.php
@@ -3,8 +3,8 @@
         <div style="display: flex; flex-direction: row; align-items: center; margin-top: 6px;">
             <h3 style="text-align: center; font-weight: 400; color: var(--text-color); margin: 0; padding: 12px;">View and Download <span id="filename"></span></h3>
             <a id="modal-download">
-                <div style="background: var(--main-color); border-radius: 30px; margin: auto 16px auto auto; padding: 6px;">
-                    <iron-icon icon="icons:file-download" style="color: white; max-width: 20px; max-height: 20px;"></iron-icon>
+                <div style="background: var(--main-color); border-radius: 30px;">
+                    <iron-icon icon="icons:file-download" style="color: white; width: 20px; height: 20px; margin: 8px;"></iron-icon>
                 </div>                
             </a>
         </div>        
